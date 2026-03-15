ba8c5ac2e76a7221b22fa92755d2f532b513414d
abdirisak <abdirisak@jamil.com>
2026-03-15 03:18:26 -1100

    create student class with private fields and constructor

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+      // 1. Private fields (Encapsulation)
+    private String name;
+    private int studentId;
+    
+    // 2. Constructor - initializes the fields
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+}
