cd015570d35585a2244fb2bc0cb244aa444d1912
abdirisak <abdirisak@jamil.com>
2026-03-15 03:19:45 -1100

    Added getters and setters for encapsulation

--- src/Student.java
+++ src/Student.java
@@ -8,4 +8,23 @@ public class Student {
         this.name = name;
         this.studentId = studentId;
     }
+        // 3. Getter for name
+    public String getName() {
+        return name;
+    }
+    
+    // 4. Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    // 5. Getter for studentId
+    public int getStudentId() {
+        return studentId;
+    }
+    
+    // 6. Setter for studentId
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 }
