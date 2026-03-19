c8bfb40105b249416dd1b18f4a1549686d98218e
abdirasakidris638-ship-it <abdirasakidris638@gmail.com>
2026-03-19 02:09:24 -1100

    compelete-main-logic
    compelete the main logic of my program

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-  // Create two Student objects
-        Student student1 = new Student("Alice Johnson", 1001);
-        Student student2 = new Student("Bob Smith", 1002);
-        
-        // Print their names
-        System.out.println("Student 1: " + student1.getName());
-        System.out.println("Student 2: " + student2.getName());
-        
-        // Optional: Test setters
-        student1.setName("Alice Williams");
-        System.out.println("Updated Student 1: " + student1.getName());
-
-
-
-
-        // Creating Student objects
+  // Creating Student objects
         Student student1 = new Student("Abdirasak", 1001);
         Student student2 = new Student("Amina", 102);
 
